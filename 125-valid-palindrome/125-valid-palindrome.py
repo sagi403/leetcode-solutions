b79a2242fcb3bcf511b7233351befb73bd7e5278
Sagi Levi <95089762+sagi403@users.noreply.github.com>
2022-08-17 10:33:49 +0300

    Time: 104 ms (18.31%), Space: 19.9 MB (5.02%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -2,9 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         new_s = []
         for ch in s:
-            if (ord("A") <= ord(ch) <= ord("Z") or
-                ord("a") <= ord(ch) <= ord("z") or
-                ord("0") <= ord(ch) <= ord("9")):
+            if ch.isalnum():
                 new_s.append(ch.lower())
         for i in range(len(new_s) - 1):
             if new_s[i] != new_s[-i-1]:
